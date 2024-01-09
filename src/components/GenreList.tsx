fcf3e824057f8881334b59aa18aac5defb483afc
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2024-01-09 12:25:46 -0700

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Skeleton, Spinner, Text } from '@chakra-ui/react';
+import { Button, HStack, Heading, Image, List, ListItem, Skeleton, Spinner, Text } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenre'
 import getCroppedImageUrl from '../services/image-url';
 
@@ -14,19 +14,25 @@ const GenreList = ({onSelectGenre,selectedGenre} :Props) => {
   if(isLoading) return <Skeleton height='100vh' width='100%' borderRadius='5px'></Skeleton>
 
   return (
-    <List>
-      {data.map(genre => 
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
-            <Image boxSize='32px' borderRadius='5px' src={getCroppedImageUrl(genre.image_background)}/>
-            <Button 
-              fontWeight={genre.id === selectedGenre?.id ? 'bold':'normal'} 
-              onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>
-                { genre.name }
-            </Button>
-          </HStack>
-        </ListItem>)}
-    </List>
+    <>
+      <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+      <List>
+        {data.map(genre => 
+          <ListItem key={genre.id} paddingY='5px'>
+            <HStack>
+              <Image 
+                objectFit='cover' boxSize='32px' borderRadius='5px' 
+                src={getCroppedImageUrl(genre.image_background)}/>
+              <Button 
+                whiteSpace='normal' textAlign='left'
+                fontWeight={genre.id === selectedGenre?.id ? 'bold':'normal'} 
+                onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>
+                  { genre.name }
+              </Button>
+            </HStack>
+          </ListItem>)}
+      </List>
+    </>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,7 +12,7 @@ const NavBar = ({onSearch}:Props) => {
     <HStack padding='10px'>
         <Image src={logo} boxSize='60px' ></Image>
         <SearchInput onSearch={onSearch}/>
-        <ColorModeSwitch/>
+        <ColorModeSwitch />
     </HStack>
   )
 }
