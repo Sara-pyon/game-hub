3bcf949de0a1d4be2813f297096844e187057362
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2024-01-08 17:40:03 -0700

    Build a sort selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -5,16 +5,19 @@ import { Platform } from '../hooks/useGames'
 
 interface Props{
     onSelectedPlatform: (platform: Platform) => void;
+    selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({onSelectedPlatform}:Props) => {
+const PlatformSelector = ({onSelectedPlatform, selectedPlatform}:Props) => {
   const {data, error} = usePlatforms();
 
   if(error) return null;
     
   return (
     <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}>PlatForm</MenuButton>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          {selectedPlatform?.name || 'Platforms'}
+        </MenuButton>
         <MenuList>
             {data.map(platform => 
                 <MenuItem key={platform.id} onClick={() => onSelectedPlatform(platform)}>
